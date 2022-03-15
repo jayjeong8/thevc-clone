dde1d6a856090f9fa5937ea4d873729a82d57356
jay <zuni4242@gmail.com>
2022-03-16 01:23:35 +0900

    :lipstick: style theme 수정

--- src/style.d.ts
+++ src/style.d.ts
@@ -3,11 +3,26 @@ import "styled-components";
 declare module "styled-components" {
     export interface DefaultTheme {
         black: string;
+        blue:{
+            deep: string;
+            dark: string;
+            main: string;
+        };
         gray: {
             dark: string;
             gray: string;
             light: string;
         };
-        white: string
+        white: string;
+        type: {
+            L: string;
+            M: string;
+            S: string;
+        };
+        gap: {
+            L: string;
+            M: string;
+            S: string;
+        };
     }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,27 @@
 import {DefaultTheme} from "styled-components";
 
 export const theme: DefaultTheme = {
-    black: "#121212",
-    gray: {
-        dark: "#414137",
-        gray: "#97968A",
-        light: "#DDDDD6",
-    },
-    white: "#F9F9F8",
-
-};
+        black: "#000000",
+        blue: {
+            deep: "#101318",
+            dark: "#181d24",
+            main: "#262e3a",
+        },
+        gray: {
+            dark: "#414137",
+            gray: "#97968A",
+            light: "#DDDDD6",
+        },
+        white: "#ffffff",
+        type: {
+            L: "1.4rem",
+            M: "1rem",
+            S: "0.75rem"
+        },
+        gap: {
+            L: "16px",
+            M: "8px",
+            S: "4px",
+        }
+    }
+;
