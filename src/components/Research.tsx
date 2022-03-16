6ff3d1e4e3382e5ef886079670a5c7d280e23f73
jay <zuni4242@gmail.com>
2022-03-17 00:37:28 +0900

    :poop: lazy loading test 1

--- src/components/Research.tsx
+++ src/components/Research.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import {LazyLoadingSection, ResearchInfo, ResearchTitle, ResearchWrapper} from "../style/ResearchStyle";
-
+import {InvestImage, LazyLoadingSection, ResearchInfo, ResearchTitle, ResearchWrapper} from "../style/ResearchStyle";
+import {useQuery} from "react-query";
+import {IImageData} from "../../interface";
 
 export default function Research() {
+    const getImageData = () => {
+        return fetch('http://localhost:3000/data/investmentImageData.json')
+            .then(response => response.json())
+    }
+    const {data} = useQuery<IImageData[]>(
+        "imageData", getImageData
+    );
     return (
         <ResearchWrapper>
             <ResearchTitle>투자/M&A 탐색</ResearchTitle>
@@ -17,7 +25,13 @@ export default function Research() {
                 </ResearchInfo>
             </section>
             <LazyLoadingSection>
-
+                {data?.map((data: IImageData ) => (
+                    <InvestImage>
+                        <img loading="lazy"
+                             src={data.image}
+                             alt="Investment"/>
+                    </InvestImage>
+                ))}
             </LazyLoadingSection>
         </ResearchWrapper>
     )
